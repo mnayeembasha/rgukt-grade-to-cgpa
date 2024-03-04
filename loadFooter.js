6684bc2541ab592fa967b6583c22cb2a3c138936
Nayeem Basha <mnayeembasha@gmail.com>
2024-03-04 22:49:13 +0530

    Update loadFooter.js

--- loadFooter.js
+++ loadFooter.js
@@ -3,7 +3,7 @@
     const footerContainer = document.querySelector('footer');
 
     // Fetch footer.html content
-    fetch('http://localhost:8080/footer.html')
+    fetch('/footer.html')
         .then(response => response.text())
         .then(html => {
             // Append the header content to the container
