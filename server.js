d4da342347e6ac3e922988590570c9c1d0dc62f1
mnayeembasha <mnayeembasha@gmail.com>
2024-03-04 22:06:21 +0530

    Added files

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 
 // app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname)));
 
-<<<<<<< HEAD
-// Define a route to handle the root URL
-=======
 //Define a route to handle the root URL
->>>>>>> 2c0a025 (Added files)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,  'index.html'));
 });
